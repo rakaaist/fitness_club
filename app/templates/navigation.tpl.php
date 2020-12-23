b6c6944f65baed8966100f9e60286999184420c1
rakaaist <rakaaist@gmail.com>
2020-12-23 10:22:58 +0200

    Add completed project with updated active state to navigation menu

--- app/templates/navigation.tpl.php
+++ app/templates/navigation.tpl.php
@@ -3,14 +3,7 @@
         <div class="nav-side">
 
             <?php foreach ($data['left'] as $link => $title): ?>
-                <li class="
-                <?php if ($_SERVER['REQUEST_URI'] == ('/' . strtolower($title))): ?>
-                    <?php print 'active'; ?>
-                <?php elseif (($_SERVER['REQUEST_URI'] == ('/index') || $_SERVER['REQUEST_URI'] == ('/'))
-                    && $title === 'Home'): ?>
-                    <?php print 'active'; ?>
-                <?php endif; ?>
-                ">
+                <li class="<?php print ($_SERVER['REQUEST_URI'] == $link) ? 'active' : ''; ?>">
                     <a href="<?php print $link; ?>"><?php print $title; ?></a>
                 </li>
             <?php endforeach; ?>
@@ -20,11 +13,7 @@
         <div class="nav-side">
 
             <?php foreach ($data['right'] as $link => $title): ?>
-                <li class="nav-list
-                <?php if ($_SERVER['REQUEST_URI'] == ('/' . strtolower($title)) || $_SERVER['REQUEST_URI'] == ('')): ?>
-                    <?php print 'active'; ?>
-                <?php endif; ?>
-                ">
+                <li class="nav-list <?php print ($_SERVER['REQUEST_URI'] == $link) ? 'active' : ''; ?>">
                     <a href="<?php print $link; ?>"><?php print $title; ?></a>
                 </li>
             <?php endforeach; ?>
